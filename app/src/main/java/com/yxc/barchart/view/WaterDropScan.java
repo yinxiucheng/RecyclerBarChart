aff6acd32b15dfc8adcc89125225bc4323dfcfcc
yinxiucheng <yinxiucheng@gmail.com>
2019-04-30 19:40:35 +0800

    "扫光单独分出来"

--- app/src/main/java/com/yxc/barchart/view/WaterDropScan.java
+++ app/src/main/java/com/yxc/barchart/view/WaterDropScan.java
@@ -1,7 +1,6 @@
 package com.yxc.barchart.view;
 
 import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.content.Context;
@@ -22,16 +21,7 @@ import java.util.List;
  * @author yxc
  * @since 2019/4/28
  */
-public class WaterDrop extends FrameLayout {
-
-    BezierCircle water1;
-    BezierCircle water2;
-    BezierCircle water3;
-    BezierCircle water4;
-    BezierCircle water5;
-    BezierCircle water6;
-    BezierCircle water7;
-    BezierCircle water8;
+public class WaterDropScan extends FrameLayout {
 
     BezierCircle waterScan1;
     BezierCircle waterScan2;
@@ -42,7 +32,6 @@ public class WaterDrop extends FrameLayout {
     BezierCircle waterScan7;
     BezierCircle waterScan8;
 
-    AnimatorSet mAnimatorLevelSet;
     AnimatorSet mAnimatorScanSet;
 
     Animator wrapAnimator;
@@ -75,55 +64,29 @@ public class WaterDrop extends FrameLayout {
         ;
     };
 
-    public WaterDrop(Context context) {
+    public WaterDropScan(Context context) {
         super(context);
         this.mContext = context;
         initView();
     }
 
-    public WaterDrop(Context context, @Nullable AttributeSet attrs) {
+    public WaterDropScan(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         this.mContext = context;
         initView();
     }
 
-    public WaterDrop(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    public WaterDropScan(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         this.mContext = context;
         initView();
     }
 
     private void initView() {
-        initBgView();
         initWaterScanView();
-        initWaterDropView();
-        initAnimationLevelSet();
         initAnimationScanSet();
         initWrapAnimator();
         startScanAnimator();
-        startLevelAnimator();
-    }
-
-    public void initBgView() {
-        this.addWaterColorful(8, R.color.water_n_drop8);
-        this.addWaterColorful(7, R.color.water_n_drop7);
-        this.addWaterColorful(6, R.color.water_n_drop6);
-        this.addWaterColorful(5, R.color.water_n_drop5);
-        this.addWaterColorful(4, R.color.water_n_drop4);
-        this.addWaterColorful(3, R.color.water_n_drop3);
-        this.addWaterColorful(2, R.color.water_n_drop2);
-        this.addWaterColorful(1, R.color.water_n_drop1);
-    }
-
-    public void initWaterDropView() {
-        water1 = createWaterDrop(1, R.color.water_drop1);
-        water2 = createWaterDrop(2, R.color.water_drop2);
-        water3 = createWaterDrop(3, R.color.water_drop3);
-        water4 = createWaterDrop(4, R.color.water_drop4);
-        water5 = createWaterDrop(5, R.color.water_drop5);
-        water6 = createWaterDrop(6, R.color.water_drop6);
-        water7 = createWaterDrop(7, R.color.water_drop7);
-        water8 = createWaterDrop(8, R.color.water_drop8);
     }
 
 
@@ -146,16 +109,6 @@ public class WaterDrop extends FrameLayout {
         waterScan8 = createWaterDrop(8, R.color.water_drop8);
     }
 
-    public void resetWaterDrop() {
-        removeView(water1);
-        removeView(water2);
-        removeView(water3);
-        removeView(water4);
-        removeView(water5);
-        removeView(water6);
-        removeView(water7);
-        removeView(water8);
-    }
 
     public void resetWaterScan() {
         removeView(waterScan1);
@@ -189,26 +142,6 @@ public class WaterDrop extends FrameLayout {
         handler.sendEmptyMessageDelayed(0, 2000);
     }
 
-    //层变动画
-    public void startLevelAnimator() {
-        water1.setAlpha(0.f);
-        addView(water1);
-        water2.setAlpha(0.f);
-        addView(water2);
-        water3.setAlpha(0.f);
-        addView(water3);
-        water4.setAlpha(0.f);
-        addView(water4);
-        water5.setAlpha(0.f);
-        addView(water5);
-        water6.setAlpha(0.f);
-        addView(water6);
-        water7.setAlpha(0.f);
-        addView(water7);
-        water8.setAlpha(0.f);
-        addView(water8);
-        mAnimatorLevelSet.start();
-    }
 
 
     private ObjectAnimator createLevelAnimator(final BezierCircle water1, long delay) {
@@ -221,34 +154,6 @@ public class WaterDrop extends FrameLayout {
     }
 
     //层变动画
-    public void initAnimationLevelSet() {
-        mAnimatorLevelSet = new AnimatorSet();
-        ObjectAnimator water1Alpha = createLevelAnimator(water1, 0);
-        ObjectAnimator water2Alpha = createLevelAnimator(water2, 1 * 2000);
-        ObjectAnimator water3Alpha = createLevelAnimator(water3, 2 * 2000);
-        ObjectAnimator water4Alpha = createLevelAnimator(water4, 3 * 2000);
-        ObjectAnimator water5Alpha = createLevelAnimator(water5, 4 * 2000);
-        ObjectAnimator water6Alpha = createLevelAnimator(water6, 5 * 2000);
-        ObjectAnimator water7Alpha = createLevelAnimator(water7, 6 * 2000);
-        ObjectAnimator water8Alpha = createLevelAnimator(water8, 7 * 2000);
-        List<Animator> list = new ArrayList<>();
-        list.add(water1Alpha);
-        list.add(water2Alpha);
-        list.add(water3Alpha);
-        list.add(water4Alpha);
-        list.add(water5Alpha);
-        list.add(water6Alpha);
-        list.add(water7Alpha);
-        list.add(water8Alpha);
-        mAnimatorLevelSet.playTogether(list);
-        mAnimatorLevelSet.addListener(new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                super.onAnimationEnd(animation);
-                handler.sendEmptyMessage(1);
-            }
-        });
-    }
 
 
     private ObjectAnimator createScanAnimator(BezierCircle water1, long delay, long duration) {
