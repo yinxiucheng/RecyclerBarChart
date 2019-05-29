aa2fc5c2e6f30e0cb1e8afa50b64c46830d3be45
yinxiucheng <yinxiucheng@gmail.com>
2019-05-29 10:16:17 +0800

    "自定义ViewPager2 空间，添加LineLayoutManager的reverseLayout"

--- app/src/main/java/com/yxc/barchart/ui/sleep/SleepActivity.java
+++ app/src/main/java/com/yxc/barchart/ui/sleep/SleepActivity.java
@@ -1,14 +1,20 @@
 package com.yxc.barchart.ui.sleep;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
 import android.view.MenuItem;
 import android.widget.FrameLayout;
 
 import com.yxc.barchart.R;
+import com.yxc.widgetlib.viewpager2.ViewPager2;
 import com.yxc.commonlib.util.ColorUtil;
 import com.yxc.commonlib.util.TimeUtil;
 import com.yxc.widgetlib.calendar.view.DayCalendarView;
@@ -23,9 +29,10 @@ public class SleepActivity extends AppCompatActivity implements DayCalendarView.
 
     Toolbar toolbar;
     FrameLayout container;
-    SleepFragment currentFragment;
     LocalDate mLocalDate;
     DayCalendarView mCalendarView;
+
+    ViewPager2 viewPager2;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -34,10 +41,14 @@ public class SleepActivity extends AppCompatActivity implements DayCalendarView.
         initView();
     }
 
+    ViewPagerAdapter mPageAdapter;
+
     private void initView() {
 
+        mLocalDate = LocalDate.now();
+
         toolbar = findViewById(R.id.toolBar);
-        toolbar.setTitle(TimeUtil.getDateStr(TimeUtil.localDateToTimestamp(LocalDate.now()), "M月dd日"));
+        toolbar.setTitle(TimeUtil.getDateStr(TimeUtil.localDateToTimestamp(mLocalDate), "M月dd日"));
         toolbar.setNavigationIcon(R.drawable.ic_chevron_left_white_45dp);
         toolbar.setTitleTextColor(ColorUtil.getResourcesColor(this, R.color.white));
         setSupportActionBar(toolbar);
@@ -45,8 +56,29 @@ public class SleepActivity extends AppCompatActivity implements DayCalendarView.
         mCalendarView = findViewById(R.id.calendar);
         mCalendarView.setOnDayCalendarItemSelectListener(this);
         container = findViewById(R.id.container);
-        mLocalDate = LocalDate.now();
-        switchTab(SleepFragment.class, "SleepFragment", mLocalDate);
+
+        mPageAdapter = new ViewPagerAdapter(this, mLocalDate);
+        viewPager2 = findViewById(R.id.view_pager);
+        viewPager2.setAdapter(mPageAdapter);
+        viewPager2.setReverseLayout(true);
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                super.onPageScrollStateChanged(state);
+            }
+        });
+
     }
 
     @Override
@@ -58,27 +90,6 @@ public class SleepActivity extends AppCompatActivity implements DayCalendarView.
         return super.onOptionsItemSelected(item);
     }
 
-    public void switchTab(Class clz, String tag, LocalDate localDate) {
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        SleepFragment fragment = (SleepFragment) getSupportFragmentManager().findFragmentByTag(tag);
-        if (fragment == null) {
-            try {
-                fragment = (SleepFragment) clz.newInstance();
-                fragment.setArguments(bindTimestamp(TimeUtil.localDateToTimestamp(localDate)));
-                ft.add(R.id.container, fragment, tag);
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            } catch (InstantiationException e) {
-                e.printStackTrace();
-            }
-        } else {
-            fragment.setArguments(bindTimestamp(TimeUtil.localDateToTimestamp(localDate)));
-            ft.show(fragment);
-        }
-        ft.commitAllowingStateLoss();
-        currentFragment = fragment;
-    }
-
     private Bundle bindTimestamp(long timestamp){
         Bundle bundle = new Bundle();
         bundle.putLong("timestamp", timestamp);
@@ -87,7 +98,38 @@ public class SleepActivity extends AppCompatActivity implements DayCalendarView.
 
     @Override
     public void onDayItemSelect(LocalDate localDate) {
-        switchTab(SleepFragment.class, "SleepFragment", localDate);
+//        switchTab(SleepFragment.class, "SleepFragment", localDate);
+    }
+
+
+    class ViewPagerAdapter extends FragmentStateAdapter{
+        LocalDate mLocalDate;
+        public ViewPagerAdapter(@NonNull FragmentActivity fragmentActivity, LocalDate localDate) {
+            super(fragmentActivity);
+            this.mLocalDate = localDate;
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            LocalDate localDate = mLocalDate.minusDays(position);
+            Class clz = SleepFragment.class;
+            SleepFragment fragment = null;
+            try {
+                fragment = (SleepFragment) clz.newInstance();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            }
+            fragment.setArguments(bindTimestamp(TimeUtil.localDateToTimestamp(localDate)));
+            return fragment;
+        }
+
+        @Override
+        public int getItemCount() {
+            return 1000;
+        }
     }
 
 
